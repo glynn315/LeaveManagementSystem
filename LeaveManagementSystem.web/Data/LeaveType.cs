8734acbb296eeeb20fe5d34a1a737d93d390641f
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-02-19 01:31:35 +0800

    Add Leave Type Funtion

---
+++ LeaveManagementSystem.web/Data/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(150)")]
+        public string LeaveTypeName { get; set; }
+        public int NumberOfDays { get; set; }
+
+    }
+}
